39eaa5e96f9609d4fe61daef153fae4785332879
rgab1 <rivaultgabin@gmail.com>
2026-04-08 21:08:12 +0200

    feat: wip add the movement functions

---
+++ Utils/moves.c
@@ -0,0 +1,11 @@
+#include <so_long.h>
+
+void	move_up(t_game *game)
+{
+	if (game->p_x < 2)
+		return ;
+	if (game->map[game->p_x - 1][game[p_y]] != '1')
+	{
+		if (game->map[game->p_x - 1][game[p_y]] == )
+	}
+}
